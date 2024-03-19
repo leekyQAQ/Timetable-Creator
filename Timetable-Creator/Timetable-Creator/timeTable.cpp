13a9593b9eafb59f13ea0f588793ccc118556516
leekyq <zhaobeier2023@163.com>
2024-03-19 11:10:46 -0400

    timetable function1
    save function (need fix)
    constructor
    confilct(in process)

--- Timetable-Creator/Timetable-Creator/timeTable.cpp
+++ Timetable-Creator/Timetable-Creator/timeTable.cpp
@@ -1,1 +1,31 @@
 #include "timeTable.h"
+timeTable::timeTable(vector<event >timetable)
+{
+	m_timeTable = timetable;
+}
+bool timeTable::timeConfilctCheck()
+{
+	for (int day = 1; day <= 7; day++)
+	{
+		int numberOfEventInDay;
+		for (event num : m_timeTable)
+		{
+			if (num.getEventDay() == day)
+			{
+				numberOfEventInDay++;
+			}
+		}
+		for (int i = 0; i <= numberOfEventInDay; i++)
+		{
+
+		}
+	}
+}
+void timeTable::saveTimetable(fstream& file)
+{
+	for (int i = 0; i = m_timeTable.size(); i++)
+	{
+		m_timeTable[i].saveEvent(file);
+
+	}
+}
--- Timetable-Creator/Timetable-Creator/timeTable.h
+++ Timetable-Creator/Timetable-Creator/timeTable.h
@@ -1,6 +1,13 @@
 #pragma once
+#include"event.h"
 class timeTable
 {
-	
-};
+protected:
+	vector<event> m_timeTable;
+public:
+	timeTable(vector<event >timetable);
+	bool timeConfilctCheck();
+	void saveTimetable(fstream& file);
 
+};
+ostream& operator<<(ostream& s, timeTable v);
