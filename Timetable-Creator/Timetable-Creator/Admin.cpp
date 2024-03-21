c22c77a1b93c3691857e2279f488267340bd8f22
Inabazen <163797040+Inabazen@users.noreply.github.com>
2024-03-21 11:23:16 -0400

    Admin

--- Timetable-Creator/Timetable-Creator/Admin.cpp
+++ Timetable-Creator/Timetable-Creator/Admin.cpp
@@ -1,1 +1,65 @@
 #include "Admin.h"
+void Admin::log_in()
+{
+	int lineNum = 0;
+	string ID, pass, IDhold, passHold;
+
+	//count the number of line in the file
+	ifstream file_in("AdminID.txt");
+	while (!file_in.eof())
+	{
+		getline(file_in, IDhold);
+		lineNum++;
+	}
+	file_in.close();
+
+	//check the username
+	ifstream file_in("AdminID.txt");
+	cout << "Enter the user ID: ";
+	cin >> ID;
+	while (!file_in.eof())
+	{
+
+	}
+
+
+}
+
+void Admin::sign_in()
+{
+	string hold;
+	bool check = true;
+
+	cout << "Creat the User ID" << endl;
+	cout << "Please enter your user ID: ";
+	cin >> m_id;
+	cout << "Make the password" << endl;
+	do {
+		cout << "Please Enter the Password: ";
+		cin >> pass;
+		cout << "Please Re-Enter your password: ";
+		cin >> hold;
+		if (pass != hold)
+		{
+			check = false;
+			cout << "Password does't mach. Try again." << endl;
+		}
+	} while (!check);
+	
+	ofstream file_out("AdminID.txt");
+	file_out << ID << " " << pass << endl;
+	file_out.close();
+}
+
+void Admin::addID()
+{
+
+}
+void Admin::addCourses()
+{
+
+}
+void Admin::addMeeting()
+{
+
+}
--- Timetable-Creator/Timetable-Creator/Admin.h
+++ Timetable-Creator/Timetable-Creator/Admin.h
@@ -2,9 +2,6 @@
 #include "User.h"
 class Admin : public User
 {
-protected:
-	int m_id;
-	string m_pass;
 public:
 	void log_in();
 	void sign_in();
