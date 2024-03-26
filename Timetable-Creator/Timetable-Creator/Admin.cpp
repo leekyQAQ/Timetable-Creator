97b6c7e13b02b7b28a00c9772853adac5f03b309
Inabazen <163797040+Inabazen@users.noreply.github.com>
2024-03-26 10:59:23 -0400

    Admin almost finished

--- Timetable-Creator/Timetable-Creator/Admin.cpp
+++ Timetable-Creator/Timetable-Creator/Admin.cpp
@@ -1,103 +1,64 @@
 #include "Admin.h"
+
 void Admin::login()
 {
-	int ID, decision;
-	string pass;
-	bool check = false;
-	ifstream file_in("AdminID.txt");
-
-	do {
-		cout << "Enter the user ID: ";
-		cin >> ID;
-		while (!check)
-		{
-			file_in >> m_id >> m_password;
-			if (m_id == ID)
-				check = true;
-
-			else if (file_in.eof())
-			{
-				cout << "There is no user ID maches." << endl;
-				cout << "1. Try again." << endl;
-				cout << "2. Sign in." << endl;
-				cin >> decision;
-				if (decision == 2)
-					signin();
-				
-				break;
-			}
-		}
-	} while (!check);
-
-	file_in.close();
-
-	do
-	{
-		cout << "Enter the password: ";
-		cin >> pass;
-		if (pass != m_password)
-			cout << "Password is wrong. Please try again." << endl << endl;
-	} while (pass != m_password);
-	
-
-
 }
 
-void Admin::signin()
-{
-	string hold;
-	int holdID;
-	bool check = true;
 
-	cout << "Creat the User ID" << endl;
-	cout << "Please enter your user ID: ";
-	cin >> m_id;
-
-	ifstream file_in("AdminID.txt");
-	while (!file_in.eof())
-	{
-		file_in >> holdID >> hold;
-		if (m_id == holdID)
-		{
-			cout << "Your account already exist." << endl;
-			check = false;
-			break;
-		}
-	}
-
-	
-	if (check) //if account doesn't exist
-	{
-		cout << "Make the password" << endl;
-		do {
-			cout << "Please Enter the Password: ";
-			cin >> m_password;
-			cout << "Please Re-Enter your password: ";
-			cin >> hold;
-			if (m_password != hold)
-			{
-				check = false;
-				cout << "Password does't mach. Try again." << endl << endl;
-			}
-		} while (!check);
-	}
-}
 
 void Admin::saveInfo()
 {
 	ofstream file_out("AdminID.txt");
 	file_out << m_id << " " << m_password << endl;
+	file_out.close();
 }
 
 void Admin::addID()
 {
+	int ID, hold;
+	string filename;
+	cout << "Enter 1 for student ID and 2 for teacher ID: ";
+	cin >> hold;
+	if (hold == 1)
+		filename = "";
+	else
+		filename = "";
 
+	cout << "Enter the ID: ";
+	cin >> ID;
+	ofstream file_out(filename);
+	file_out << ID << endl;
+	file_out.close();
 }
+
 void Admin::addCourses()
 {
+	int code;
+	string name;
+	vector <int> participants;
+	cout << "Enter the course code: ";
+	cin >> code;
+	cout << "Enter the course name: ";
+	cin >> name;
 
+	event course(code, name, participants);
+	fstream file_out("Event.txt");
+	course.saveEvent(file_out);
+	file_out.close();
 }
+
 void Admin::addMeeting()
 {
+	int code;
+	string name;
+	vector <int> participants;
+	cout << "Enter the meeeting code: ";
+	cin >> code;
+	cout << "Enter the meeting name: ";
+	cin >> name;
 
+	event course(code, name, participants);
+	fstream file_out("Event.txt");
+	course.saveEvent(file_out);
+	file_out.close();
 }
--- Timetable-Creator/Timetable-Creator/Admin.h
+++ Timetable-Creator/Timetable-Creator/Admin.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "User.h"
+#include "Event.h"
 class Admin : public User
 {
 public:
 	Admin(){}
 	void login();
-	void signin();
 	void addID();
 	void addCourses();
 	void addMeeting();
