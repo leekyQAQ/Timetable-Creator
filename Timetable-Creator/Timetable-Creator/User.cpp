aeea13e536700edf2b2bcb2bee666267d7cfa595
Moses Lei <111721479+Moses3424@users.noreply.github.com>
2024-03-27 22:56:36 -0400

    Additional main function and Login section update.

--- Timetable-Creator/Timetable-Creator/User.cpp
+++ Timetable-Creator/Timetable-Creator/User.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 User::User() {
 	m_password = "";
@@ -48,38 +50,56 @@ void User::login(fstream& file) {
 }
 
 
+
 void User::signin(fstream& file) {
-	int inputId;
-	/// ask the user to enter their id The id must already exist in the file
-	cout << "Enter your user ID: ";
-	cin >> inputId;
+    // Variable to store the input ID
+    int inputId;
 
-	// Check if the ID already exists in the file if not tell them that the id is invalid and try again
-	int storedId;
-	string storedPassword;
-	bool idExists = false;
+    // Ask the user to enter their ID
+    cout << "Enter your user ID: ";
+    cin >> inputId;
 
-	while (file >> storedId >> storedPassword) {
-		if (storedId == inputId) {
-			idExists = true;
-			break;
-		}
-	}
-	if (!idExists) {
-		cout << "User ID does not exists. Please choose a different ID." << endl;
-		return;
-	}
+    // Vectors to store user IDs and passwords
+    vector<int> ids;
+    vector<string> passwords;
 
-	// if the id exist in the file and don't have an passwrod attach to it than Ask the user to create a password
-	string newPassword;
-	cout << "Create a password: " ;
-	cin >> newPassword;
+    // Variables to store the ID and password read from the file
+    int storedId;
+    string storedPassword;
 
-	// Save the new user to the file make sure the the password is linked prited beside the user id since there will be many id with no password attached to it 
-	ofstream file_out(file, ios_base::app);
-	file_out << inputId << " " << newPassword << endl;
-	file_out.close();
+    // Flag to check if the input ID exists in the file
+    bool idExists = false;
 
-	cout << "Sign-up successful! You can now log in." << endl;
+    // Read the IDs and passwords from the file into the vectors
+    while (file >> storedId >> storedPassword) {
+        ids.push_back(storedId);  // Add the ID to the IDs vector
+        passwords.push_back(storedPassword);  // Add the password to the passwords vector
+        if (storedId == inputId) {
+            idExists = true;  // If the ID exists in the file, set the flag to true
+        }
+    }
 
+    // If the input ID does not exist in the file, print an error message and return
+    if (!idExists) {
+        cout << "User ID does not exist. Please choose a different ID." << endl;
+        return;
+    }
+
+    // Ask the user to create a new password
+    string newPassword;
+    cout << "Create a new password: ";
+    cin >> newPassword;
+
+    // Loop through the IDs vector
+    for (int i = 0; i < ids.size(); i++) {
+        // If the ID is the input ID, update the password in the passwords vector
+        if (ids[i] == inputId) {
+            passwords[i] = newPassword;
+        }
+        // Write the ID and password to the file
+        file << ids[i] << " " << passwords[i] << endl;
+    }
+
+    // Print a success message
+    cout << "Password Set successfully. You can now log in." << endl;
 }
--- Timetable-Creator/Timetable-Creator/main.cpp
+++ Timetable-Creator/Timetable-Creator/main.cpp
@@ -9,14 +9,21 @@ int main() {
     // Open the file
     int choice;
     cout << "Welcome to the program" << endl;
-    cout << "" << endl;
+    cout << "Are you 1)Student, 2)Teacher, 3)Admin" << endl;
+    cin >> choice;
+    if (choice == 1) {
+        fstream file("Studentdata.txt");
+        User user;
+        cout << "Login(1)/ Signup(2)" << endl;
+        cin >> choice;
+        if (choice == 1){
+            user.login(file);
+        }
+        else {
+            user.signin(file);
+        }
+        file.close();
+    }
 
-    fstream file("Studentdata.txt");
-    User user;
-   
-    user.signin(file);
-    user.login(file);
-
-    file.close();
 
 }
